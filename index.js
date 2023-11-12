97b2df825d2400354e90188c252d8fcddeba10b9
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-11-12 14:44:12 +0500

    Routing

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Main page');
+  res.status(200);
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.status(200);
+  res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
 
 const PORT = process.env.PORT || 4100;
