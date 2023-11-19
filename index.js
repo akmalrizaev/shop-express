f5de7e27ad8591ae72844ed85ea61543f301c750
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-11-19 15:32:23 +0500

    Connecting MongoDB, .env

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import { create } from 'express-handlebars';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
 import AuthRoutes from './routes/auth.js';
 import ProductsRoutes from './routes/products.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -16,10 +21,30 @@ app.set('views', './views');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(AuthRoutes);
 app.use(ProductsRoutes);
 
-const PORT = process.env.PORT || 4100;
+// const PORT = process.env.PORT || 4100;
 
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+// mongoose.set('strictQuery', false);
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+// });
+
+// app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+
+const startApp = () => {
+  try {
+    mongoose.set('strictQuery', false);
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+
+    const PORT = process.env.PORT || 4100;
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startApp();
