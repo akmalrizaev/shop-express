487e20d6ff07dfe97d13d2b57b0f2b930cdd080b
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-11-24 15:22:52 +0500

    adding connect-flash, express-session for Validation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { create } from 'express-handlebars';
 import mongoose from 'mongoose';
+import flash from 'connect-flash';
+import session from 'express-session';
 import * as dotenv from 'dotenv';
 
 import AuthRoutes from './routes/auth.js';
@@ -22,6 +24,10 @@ app.set('views', './views');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// app.use(express.cookieParser('keyboard cat'));
+// app.use(express.session({ cookie: { maxAge: 60000 } }));
+app.use(session({ secret: 'Alex', resave: false, saveUninitialized: false }));
+app.use(flash());
 
 app.use(AuthRoutes);
 app.use(ProductsRoutes);
