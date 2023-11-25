f59680ee291f125dacce6f95a193aa4cf22b693a
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-11-25 12:35:31 +0500

    Dynamic navbar, Cookie

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { create } from 'express-handlebars';
 import mongoose from 'mongoose';
 import flash from 'connect-flash';
 import session from 'express-session';
+import varMiddleware from './middleware/var.js';
+import cookieParser from 'cookie-parser';
 import * as dotenv from 'dotenv';
 
 import AuthRoutes from './routes/auth.js';
@@ -24,10 +26,12 @@ app.set('views', './views');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 // app.use(express.cookieParser('keyboard cat'));
 // app.use(express.session({ cookie: { maxAge: 60000 } }));
 app.use(session({ secret: 'Alex', resave: false, saveUninitialized: false }));
 app.use(flash());
+app.use(varMiddleware);
 
 app.use(AuthRoutes);
 app.use(ProductsRoutes);
